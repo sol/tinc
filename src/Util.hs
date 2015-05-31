68f9f6b2313880fd28b4dea78fb156d37da00551
Hoang Xuan Phu <phuhimself@phunehehe.net>
2015-05-31 22:28:19 +0800

    install dependencies with basic caching

---
+++ src/Util.hs
@@ -0,0 +1,15 @@
+module Util where
+
+import Data.List
+
+type PackageName = String
+
+parseInstallPlan :: String -> [PackageName]
+parseInstallPlan = concatMap (take 1 . words) . drop 2 . lines
+
+lookupPackage :: String -> [FilePath] -> Either String (Maybe FilePath)
+lookupPackage targetPackage packageFiles =
+  case filter ((targetPackage ++ "-") `isPrefixOf`) packageFiles of
+    [packageFile] -> return $ Just packageFile
+    [] -> return Nothing
+    multiple -> Left ("Package found multiple times: " ++ intercalate ", " multiple)
