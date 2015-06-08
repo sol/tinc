4d23b6694a55697bff05351a013c43de002ba29e
Sönke Hahn <soenkehahn@gmail.com>
2015-06-08 22:37:18 +0800

    stack-sandbox -> tinc

--- src/Run.hs
+++ src/Run.hs
@@ -1,22 +1,11 @@
-{-# LANGUAGE DeriveGeneric #-}
-
 module Run where
 
-import qualified GHC.Generics as GHC
-import           System.Console.GetOpt.Generics
+import System.Directory
+import System.FilePath
 
-import           Stack
-
-data Options
-  = Options {
-    parent :: FilePath
-  }
-  deriving (GHC.Generic)
-
-instance Generic Options
-instance HasDatatypeInfo Options
+import Stack
 
 run :: IO ()
 run = do
-  options <- getArguments
-  createStackedSandbox (Path (parent options) :: Path Sandbox)
+  home <- getHomeDirectory
+  installDependencies (Path (home </> ".tinc" </> "cache") :: Path Cache)
