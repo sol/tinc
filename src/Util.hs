1057492fe2cf10aa2e8f0d30b4bb4c16669f6a03
Sönke Hahn <soenkehahn@gmail.com>
2015-08-18 02:32:31 +0800

    remove dead code

--- src/Util.hs
+++ src/Util.hs
@@ -8,20 +8,11 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Catch
 import           Data.Char
 import           Data.List.Compat
-import qualified Data.Set as Set
 import           System.Directory
 import           System.FilePath
 
 strip :: String -> String
 strip = dropWhile isSpace . reverse . dropWhile isSpace . reverse
-
-ordNub :: (Ord a) => [a] -> [a]
-ordNub l = go Set.empty l
-  where
-    go _ [] = []
-    go s (x:xs) = if x `Set.member` s
-      then go s xs
-      else x : go (Set.insert x s) xs
 
 withCurrentDirectory :: (MonadIO m, MonadMask m) => FilePath -> m a -> m a
 withCurrentDirectory dir action = do
