e64b2125b76c015d7a94b9c34e9bb6759e22a497
Linh Nguyen <tuleism@gmail.com>
2015-08-20 11:39:56 +0800

    lock the cache

--- src/Run.hs
+++ src/Run.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE RecordWildCards #-}
 module Run where
 
+import           System.FileLock
+import           System.FilePath
 import           System.Process
 import           System.Environment.Compat
 import           Control.Exception
 
 import           Tinc.Setup
 import           Tinc.Install
+import           Tinc.Types
 
 unsetEnvVars :: IO ()
 unsetEnvVars = do
@@ -19,7 +22,16 @@ tinc args = do
   unsetEnvVars
   Facts{..} <- setup
   case args of
-    [] -> installDependencies factsGhcInfo False factsCache factsGitCache
-    ["--dry-run"] -> installDependencies factsGhcInfo True factsCache factsGitCache
+    [] -> withCacheLock factsCache $
+      installDependencies factsGhcInfo False factsCache factsGitCache
+    ["--dry-run"] -> withCacheLock factsCache $
+      installDependencies factsGhcInfo True factsCache factsGitCache
     name : rest | Just plugin <- lookup name factsPlugins -> callProcess plugin rest
     _ -> throwIO (ErrorCall $ "unrecognized arguments: " ++ show args)
+
+withCacheLock :: Path CacheDir -> IO a -> IO a
+withCacheLock cache action = do
+  putStrLn $ "Acquiring " ++ lock
+  withFileLock lock Exclusive $ \ _ -> action
+  where
+    lock = path cache </> "tinc.lock"
