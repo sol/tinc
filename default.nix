73cb7f1530774ebbb0e7ca9565ab6cc6866a3b88
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-20 20:42:40 +0800

    Delete default.nix

--- default.nix
+++
@@ -1,6 +0,0 @@
-let
-  default = { nixpkgs ? import <nixpkgs> {} }:
-    (import ./tinc.nix { inherit nixpkgs; }).resolver.callPackage ./package.nix {};
-  overrideFile = ./default-override.nix;
-  expr = if builtins.pathExists overrideFile then import overrideFile else default;
-in expr
