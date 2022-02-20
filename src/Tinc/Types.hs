19e5ae08b854b7355b6173bfbf5c8297910624f8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-20 20:03:58 +0800

    Delete src directory

--- src/Tinc/Types.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Tinc.Types where
-
-import           Data.String
-
-data CacheDir
-
-newtype Path a = Path {path :: FilePath}
-  deriving (Eq, Ord, Show, IsString)
